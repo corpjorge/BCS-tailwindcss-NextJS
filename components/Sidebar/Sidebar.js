f1460130e6cd5a27a7ad2fd52679ea56d1609ff4
Jorge Eduardo <corpjorge@hotmail.com>
2022-02-09 16:10:40 -0500

    Maquetado V1

---
+++ components/Sidebar/Sidebar.js
@@ -0,0 +1,87 @@
+import React from "react";
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+export default function Sidebar() {
+  const [collapseShow] = React.useState("hidden");
+  const router = useRouter();
+  return (
+    <>
+      <nav className="md:left-0 md:block md:fixed md:top-0 md:bottom-0 md:overflow-y-auto md:flex-row md:flex-nowrap md:overflow-hidden shadow-xl bg-blue-500 z-50 flex flex-wrap items-center justify-between relative md:w-64 py-4 px-6">
+        <div className="md:flex-col md:items-stretch md:min-h-full md:flex-nowrap px-0 flex flex-wrap items-center justify-between w-full mx-auto">
+          <button
+            className="cursor-pointer text-black opacity-50 md:hidden px-3 py-1 text-xl leading-none bg-transparent rounded border border-solid border-transparent"
+            type="button"
+          >
+            <i className="fas fa-bars" />
+          </button>
+          <Link href="/">
+            <a
+              href="#"
+              className="md:block text-left md:pb-2 text-blueGray-600 mr-0 inline-block whitespace-nowrap text-sm uppercase font-bold p-4 px-0"
+            >
+              BCS logo
+            </a>
+          </Link>
+          <div
+            className={
+              "md:flex md:flex-col md:items-stretch md:opacity-100 md:relative md:mt-4 md:shadow-none shadow absolute top-0 left-0 right-0 z-40 overflow-y-auto overflow-x-hidden h-auto items-center flex-1 rounded " +
+              collapseShow
+            }
+          >
+            <hr className="my-4 md:min-w-full" />
+            <ul className="md:flex-col md:min-w-full flex flex-col list-none">
+              <li className="items-center">
+                <Link href="/">
+                  <a
+                    href="#"
+                    className="text-xs uppercase py-3 font-bold block text-lightBlue-500 hover:text-lightBlue-600"
+                  >
+                    Dashboard
+                  </a>
+                </Link>
+              </li>
+              <li className="items-center">
+                <Link href="/">
+                  <a
+                    href="#"
+                    className="text-xs uppercase py-3 font-bold block text-blueGray-700 hover:text-blueGray-500"
+                  >
+                    Button
+                  </a>
+                </Link>
+              </li>
+
+              <li className="items-center">
+                <Link href="/tables">
+                  <a
+                    href="#"
+                    className={
+                      "text-xs uppercase py-3 font-bold block " +
+                      (router.pathname.indexOf("/admin/tables") !== -1
+                        ? "text-lightBlue-500 hover:text-lightBlue-600"
+                        : "text-blueGray-700 hover:text-blueGray-500")
+                    }
+                  >
+                    Tables
+                  </a>
+                </Link>
+              </li>
+
+              <li className="items-center">
+                <Link href="/Otros">
+                  <a
+                    href="#"
+                    className="text-xs uppercase py-3 font-bold block text-blueGray-700 hover:text-blueGray-500"
+                  >
+                    Otros
+                  </a>
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+}
---
+++ layouts/Admin.js
@@ -0,0 +1,20 @@
+import Navbar from "../components/Navbars/Navbar.js";
+import Sidebar from "../components/Sidebar/Sidebar.js";
+import Footer from "../components/Footers/Footer.js";
+import Header from "../components/Headers/Header";
+
+export default function Admin({ children }) {
+  return (
+    <>
+      <Sidebar />
+      <div className="relative md:ml-64 bg-blueGray-100">
+        <Navbar />
+        <Header />
+        <div className="px-4 md:px-10 mx-auto w-full -m-24">
+          {children}
+          <Footer />
+        </div>
+      </div>
+    </>
+  );
+}
